99b030728b14c31b622326c803f618de2d24bb34
Arthur-Jr <arthurjr1906@gmail.com>
2021-12-18 12:13:23 -0300

    Updates

---
+++ precache-manifest.a3051e50c42002b47ded904e9dd016a9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fd23fe55618550eb730dbf47bbda06",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "e80e2fbf1811f0db9b72",
+    "url": "/wallet/static/css/main.fac8b601.chunk.css"
+  },
+  {
+    "revision": "6315b5f2a07cb36c82da",
+    "url": "/wallet/static/js/2.e00f05f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.e00f05f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80e2fbf1811f0db9b72",
+    "url": "/wallet/static/js/main.6bd68e23.chunk.js"
+  },
+  {
+    "revision": "70fd6ad992cb65180c1a",
+    "url": "/wallet/static/js/runtime-main.0e753dd0.js"
+  },
+  {
+    "revision": "41c8548e2e8be499ae92af3e9a4c2be2",
+    "url": "/wallet/static/media/Delete.41c8548e.png"
+  },
+  {
+    "revision": "7ee6892c5faa9ab4f980fede5eb466db",
+    "url": "/wallet/static/media/Edit.7ee6892c.png"
+  },
+  {
+    "revision": "ebf4c60f4e27548ad5bca24ac878df6b",
+    "url": "/wallet/static/media/background-img.ebf4c60f.jpg"
+  },
+  {
+    "revision": "f25d8a38d705816c268592529e434b55",
+    "url": "/wallet/static/media/gitHubIcon.f25d8a38.svg"
+  },
+  {
+    "revision": "652200391c1a0edbd3dcd8fdf3e39a6a",
+    "url": "/wallet/static/media/linkedinIcon.65220039.svg"
+  },
+  {
+    "revision": "67b4b987d572db74bb9e53a28ec65a60",
+    "url": "/wallet/static/media/new-logo.67b4b987.png"
+  },
+  {
+    "revision": "1ba131e9574e40847dfd13d208841d64",
+    "url": "/wallet/static/media/notFound.1ba131e9.gif"
+  }
+]);
