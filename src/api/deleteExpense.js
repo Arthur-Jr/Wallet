fc8e22ef46bf8c62cd44f21691fedc66d816ac77
Arthur Junior <arthurjr1906@gmail.com>
2023-05-13 13:38:50 -0300

    fix: remove console.log

--- src/api/deleteExpense.js
+++ src/api/deleteExpense.js
@@ -5,8 +5,7 @@ const URL = process.env.REACT_APP_ENDPOINT || 'http://localhost:8080';
 const deleteExpense = async (token, id) => {
   try {
     const header = { headers: { Authorization: `Bearer ${token}` } };
-    const response = await axios.delete(`${URL}/expense/${id}`, header);
-    console.log(response);
+    await axios.delete(`${URL}/expense/${id}`, header);
   } catch (e) {
     console.log(e);
   }
